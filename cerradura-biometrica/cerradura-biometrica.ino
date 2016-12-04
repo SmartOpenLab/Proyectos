6775ffcb7ebc5e4981595291a5f6f8d0da57691f
Jorge Bote Albalá <jorikst@gmail.com>
2016-12-04 17:36:49 +0100

    Added Compilation directive and Led Information
    Added Debug compilation directive to use it with screen prints and show information by Serial.
    Added  a led module to show information with red, green and white leds for errors, pass and activity information

--- cerradura-biometrica/cerradura-biometrica.ino
+++ cerradura-biometrica/cerradura-biometrica.ino
@@ -5,15 +5,31 @@
  * @Author: Jorge Bote Albalá - Joriks <jorikst@gmail.com>
  *****************************************************************************/
 
+#define DEBUG
+
 #include <Keypad.h>
 #include <Adafruit_Fingerprint.h>
 
-//Define modules
+//Define main modules
 bool subscribe();
 bool unsubscribe();
 bool control();
 bool reset();
 bool checkFingerprint();
+
+//Define modules
+bool isFingerprintControl();
+bool getFingerprint();
+bool getPassword(int &password);
+void showLed(int led_pin, int seconds, char* message);
+
+/**************************** Relay Setup *************************************/
+#define RELAY_PIN 10 //Relay to lock frame
+
+/**************************** LEDs Setup *************************************/
+#define WHITE_LED A0 //White Led for activity
+#define GREEN_LED A1 //Red led for errors
+#define RED_LED A2 //Green led for pass
 
 /************************** Keypad Setup *************************************/
 const byte ROWS = 4; //Number of rows
@@ -39,11 +55,18 @@ SoftwareSerial mySerial(11, 12); //TTL Serial to communicate with fingerprint se
 
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial); //Fingerprint variable
 
+/**************************** IDs Setup *************************************/
+int num_control_id = 10;
+int last_control_id = 0;
+int last_id = 10;
+
 void setup(){
   Serial.begin(9600);
   kpd.setDebounceTime(50);
   kpd.addEventListener(keypadEvent);
-  
+  pinMode(WHITE_LED,OUTPUT);
+  pinMode(GREEN_LED,OUTPUT);
+  pinMode(RED_LED,OUTPUT);
   finger.begin(57600);
 }
 
@@ -53,6 +76,10 @@ void loop(){
 
 void keypadEvent(KeypadEvent eKey){
   if(kpd.getState() == PRESSED){
+    digitalWrite(WHITE_LED,HIGH);
+    #ifdef DEBUG
+      Serial.println(eKey);
+    #endif
     switch(eKey){
       case 'A':
         subscribe();
@@ -68,8 +95,9 @@ void keypadEvent(KeypadEvent eKey){
         break;
       case '*':
         checkFingerprint();
-        break;  
+        break;
     }
+    digitalWrite(WHITE_LED,LOW);
   }
 }
 
@@ -78,4 +106,12 @@ bool unsubscribe(){}
 bool control(){}
 bool reset(){}
 bool checkFingerprint(){}
+void showLed(int led_pin, int seconds, char* message){
+    #ifdef DEBUG
+      Serial.println(message);
+    #endif
+    digitalWrite(led_pin,HIGH);
+    delay(seconds*1000);
+    digitalWrite(led_pin,LOW);
+}
 
