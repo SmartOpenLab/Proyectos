3477f32250f87270d7f8d7c1cb37e27668b1b272
Antonio Gordillo <antonio.gordillo.guerrero@gmail.com>
2021-12-25 21:42:55 +0100

    Add files via upload

---
+++ esp_rfid_SOL_V1/src/src/main.cpp
@@ -0,0 +1,451 @@
+/*
+MIT License
+
+Copyright (c) 2018 esp-rfid Community
+Copyright (c) 2017 Ömer Şiar Baysal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+ #include "Arduino.h"
+ #include <ESP8266WiFi.h>
+ #include <SPI.h>
+ #include <ESP8266mDNS.h>
+ #include <ArduinoJson.h>
+ #include <FS.h>
+ #include <ESPAsyncTCP.h>
+ #include <ESPAsyncWebServer.h>
+ #include <TimeLib.h>
+ #include <Ticker.h>
+ #include "Ntp.h"
+ #include <AsyncMqttClient.h>
+ #include <Wire.h>
+ #include <Keypad_I2C.h>
+ #include <Keypad.h>
+ #include <Adafruit_NeoPixel.h>
+
+
+ #define DEBUG
+
+#ifdef OFFICIALBOARD
+
+#include <Wiegand.h>
+
+WIEGAND wg;
+int relayPin = 13;
+
+#endif
+
+#ifndef OFFICIALBOARD
+
+#include <MFRC522.h>
+#include "PN532.h"
+#include <Wiegand.h>
+#include "rfid125kHz.h"
+
+MFRC522 mfrc522 = MFRC522();
+PN532 pn532;
+WIEGAND wg;
+RFID_Reader RFIDr;
+
+int rfidss=D8;
+int readerType;
+int relayPin=D0;
+char tecla;
+char password[4]={'n','n','n','n'};
+String passReaded="aaaa";
+int pinBuzzer = D4;
+
+#endif
+
+// these are from vendors
+#include "webh/glyphicons-halflings-regular.woff.gz.h"
+#include "webh/required.css.gz.h"
+#include "webh/required.js.gz.h"
+
+// these are from us which can be updated and changed
+#include "webh/esprfid.js.gz.h"
+#include "webh/esprfid.htm.gz.h"
+#include "webh/index.html.gz.h"
+
+#ifdef ESP8266
+extern "C" {
+	#include "user_interface.h"
+}
+#endif
+
+NtpClient NTP;
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+WiFiEventHandler wifiDisconnectHandler, wifiConnectHandler;
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+unsigned long blink_ = millis();
+bool wifiFlag = false;
+bool configMode = false;
+int wmode;
+uint8_t wifipin = 255;
+#define LEDoff HIGH
+#define LEDon LOW
+
+// Variables for whole scope
+const char *http_username = "admin";
+char *http_pass = NULL;
+unsigned long previousMillis = 0;
+unsigned long previousKeypadMillis = 0;
+unsigned long previousLoopMillis = 0;
+unsigned long currentMillis = 0;
+unsigned long cooldown = 0;
+unsigned long deltaTime = 0;
+unsigned long uptime = 0;
+bool shouldReboot = false;
+bool activateKeypad = false;
+bool deactivateKeypad = false;
+bool activateRelay = false;
+bool deactivateRelay = false;
+bool inAPMode = false;
+bool isWifiConnected = false;
+unsigned long autoRestartIntervalSeconds = 0;
+
+bool wifiDisabled = true;
+bool doDisableWifi = false;
+bool doEnableWifi = false;
+bool timerequest = false;
+bool formatreq = false;
+unsigned long wifiTimeout = 0;
+unsigned long wiFiUptimeMillis = 0;
+char *deviceHostname = NULL;
+
+int mqttenabled = 0;
+char *mqttTopic = NULL;
+char *mhs = NULL;
+char *muser = NULL;
+char *mpas = NULL;
+int mport;
+
+int lockType;
+int relayType;
+unsigned long activateTime=3000;
+unsigned long activateKeypadTime=8000;
+int timeZone;
+
+unsigned long nextbeat = 0;
+unsigned long interval = 1800;
+
+//variables para el teclado
+const byte FILAS = 4;
+const byte COLUMNAS = 3;
+char teclas[FILAS][COLUMNAS] = {
+{'1','2','3'},
+{'4','5','6'},
+{'7','8','9'},
+{'*','0','#'}
+};
+
+byte PinsFilas[FILAS] = {0,1,2,3};
+byte PinsColumnas[COLUMNAS] = {4,5,6};
+int i2caddress = 0x20;
+
+Keypad_I2C kpd = Keypad_I2C( makeKeymap(teclas), PinsFilas, PinsColumnas, FILAS, COLUMNAS, i2caddress );
+
+
+#include "log.esp"
+#include "mqtt.esp"
+#include "helpers.esp"
+#include "wsResponses.esp"
+#include "rfid.esp"
+#include "wifi.esp"
+#include "config.esp"
+#include "websocket.esp"
+#include "webserver.esp"
+#include "keys.esp"
+
+#define PIN D3
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+
+void ICACHE_FLASH_ATTR setup()
+{
+#ifdef OFFICIALBOARD
+	// Set relay pin to LOW signal as early as possible
+	pinMode(13, OUTPUT);
+	digitalWrite(13, LOW);
+	delay(200);
+#endif
+
+#ifdef DEBUG
+	Serial.begin(19200);
+	Serial.println();
+
+	Serial.println(F("[ INFO ] ESP RFID v0.9"));
+
+	uint32_t realSize = ESP.getFlashChipRealSize();
+	uint32_t ideSize = ESP.getFlashChipSize();
+	FlashMode_t ideMode = ESP.getFlashChipMode();
+	Serial.printf("Flash real id:   %08X\n", ESP.getFlashChipId());
+	Serial.printf("Flash real size: %u\n\n", realSize);
+	Serial.printf("Flash ide  size: %u\n", ideSize);
+	Serial.printf("Flash ide speed: %u\n", ESP.getFlashChipSpeed());
+	Serial.printf("Flash ide mode:  %s\n", (ideMode == FM_QIO ? "QIO" : ideMode == FM_QOUT ? "QOUT" : ideMode == FM_DIO ? "DIO" : ideMode == FM_DOUT ? "DOUT" : "UNKNOWN"));
+	if (ideSize != realSize)
+	{
+		Serial.println("Flash Chip configuration wrong!\n");
+	}
+	else
+	{
+		Serial.println("Flash Chip configuration ok.\n");
+	}
+#endif
+
+	if (!SPIFFS.begin())
+	{
+#ifdef DEBUG
+		Serial.print(F("[ WARN ] Formatting filesystem..."));
+#endif
+		if (SPIFFS.format())
+		{
+			writeEvent("WARN", "sys", "Filesystem formatted", "");
+
+#ifdef DEBUG
+			Serial.println(F(" completed!"));
+#endif
+		}
+		else
+		{
+#ifdef DEBUG
+			Serial.println(F(" failed!"));
+			Serial.println(F("[ WARN ] Could not format filesystem!"));
+#endif
+		}
+	}
+	wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+	wifiConnectHandler = WiFi.onStationModeConnected(onWifiConnect);
+	configMode = loadConfiguration();
+	if (!configMode)
+	{
+		fallbacktoAPMode();
+		configMode = false;
+	}
+	else {
+		configMode = true;
+	}
+	setupWebServer();
+	writeEvent("INFO", "sys", "System setup completed, running", "");
+
+	strip.begin();
+	strip.setPixelColor(0,strip.Color(0, 0, 0));
+	strip.show(); // Initialize all pixels to 'off'
+	//parte del teclado
+	kpd.begin();  //Iniciamos el teclado
+}
+
+void ICACHE_RAM_ATTR loop()
+{
+	currentMillis = millis();
+	deltaTime = currentMillis - previousLoopMillis;
+	uptime = NTP.getUptimeSec();
+	previousLoopMillis = currentMillis;
+
+	if (wifipin != 255 && configMode && !wmode)
+	{
+		if (!wifiFlag)
+		{
+			if ((currentMillis - blink_) > 500)
+			{
+				blink_ = currentMillis;
+				digitalWrite(wifipin, !digitalRead(wifipin));
+			}
+		}
+		else
+		{
+			if (!(digitalRead(wifipin)==LEDon)) digitalWrite(wifipin, LEDon);
+		}
+	}
+
+	if (currentMillis >= cooldown)
+	{
+    passReaded="aaaa";
+    memset(password,'n',sizeof(password));
+		rfidloop();
+		strip.setPixelColor(0,strip.Color(0, 0, 0));
+		strip.show();
+	}
+
+	keypadloop();
+	if (activateKeypad&&!deactivateRelay){
+			strip.setPixelColor(0,strip.Color(0, 0, 255));
+			strip.show();
+      for (int i=0;i<4;i++){
+        password[i]=passReaded[i];
+      }
+      Serial.println(password);
+      #ifdef DEBUG
+			Serial.print("Milis : ");
+			Serial.println(millis());
+			Serial.println("activating keypad now");
+			#endif
+			previousKeypadMillis = millis();
+			activateKeypad = false;
+			deactivateKeypad = true;
+			encendido();
+	}
+
+	// Continuous relay mode
+	if (lockType == 1)
+	{
+		if (activateRelay)
+		{
+			// currently OFF, need to switch ON
+			if (digitalRead(relayPin) == !relayType)
+			{
+#ifdef DEBUG
+				Serial.print("mili : ");
+				Serial.println(millis());
+				Serial.println("activating relay now");
+#endif
+				digitalWrite(relayPin, relayType);
+			}
+			else	// currently ON, need to switch OFF
+			{
+#ifdef DEBUG
+				Serial.print("mili : ");
+				Serial.println(millis());
+				Serial.println("deactivating relay now");
+#endif
+				digitalWrite(relayPin, !relayType);
+			}
+			activateRelay = false;
+		}
+	}
+	else if (lockType == 0)	// momentary relay mode
+	{
+		if (activateRelay)
+		{
+      strip.setPixelColor(0,strip.Color(0, 255, 0));
+		strip.show();
+#ifdef DEBUG
+			Serial.print("mili : ");
+			Serial.println(millis());
+			Serial.println("activating relay now");
+#endif
+			digitalWrite(relayPin, relayType);
+			previousMillis = millis();
+			activateRelay = false;
+			deactivateRelay = true;
+		}
+		else if ((currentMillis - previousMillis >= activateTime) && (deactivateRelay))
+		{
+#ifdef DEBUG
+			Serial.println(currentMillis);
+			Serial.println(previousMillis);
+			Serial.println(activateTime);
+			Serial.println(activateRelay);
+			Serial.println("deactivate relay after this");
+			Serial.print("mili : ");
+			Serial.println(millis());
+#endif
+      digitalWrite(relayPin, !relayType);
+      deactivateRelay = false;
+      strip.setPixelColor(0,strip.Color(255, 0, 0));
+      strip.show();
+      delay(200);
+		}
+	}
+
+	if (formatreq)
+	{
+#ifdef DEBUG
+		Serial.println(F("[ WARN ] Factory reset initiated..."));
+#endif
+		SPIFFS.end();
+		ws.enable(false);
+		SPIFFS.format();
+		ESP.restart();
+	}
+
+	if (timerequest)
+	{
+		timerequest = false;
+		sendTime();
+	}
+
+	if (autoRestartIntervalSeconds > 0 && uptime > autoRestartIntervalSeconds * 1000)
+	{
+		writeEvent("INFO", "sys", "System is going to reboot", "");
+#ifdef DEBUG
+		Serial.println(F("[ WARN ] Auto restarting..."));
+#endif
+		shouldReboot = true;
+	}
+
+	if (shouldReboot)
+	{
+		writeEvent("INFO", "sys", "System is going to reboot", "");
+#ifdef DEBUG
+		Serial.println(F("[ INFO ] Rebooting..."));
+#endif
+		ESP.restart();
+	}
+
+	if (isWifiConnected)
+	{
+		wiFiUptimeMillis += deltaTime;
+	}
+
+	if (wifiTimeout > 0 && wiFiUptimeMillis > (wifiTimeout * 1000) && isWifiConnected == true)
+	{
+		writeEvent("INFO", "wifi", "WiFi is going to be disabled", "");
+		doDisableWifi = true;
+	}
+
+	if (doDisableWifi == true)
+	{
+		doDisableWifi = false;
+		wiFiUptimeMillis = 0;
+		disableWifi();
+	}
+	else if (doEnableWifi == true)
+	{
+		writeEvent("INFO", "wifi", "Enabling WiFi", "");
+		doEnableWifi = false;
+		if (!isWifiConnected)
+		{
+			wiFiUptimeMillis = 0;
+			enableWifi();
+		}
+	}
+
+	if (mqttenabled == 1)
+	{
+		if (mqttClient.connected())
+		{
+			if ((unsigned)now() > nextbeat)
+			{
+				mqtt_publish_heartbeat(now());
+				nextbeat = (unsigned)now() + interval;
+#ifdef DEBUG
+				Serial.print("[ INFO ] Nextbeat=");
+				Serial.println(nextbeat);
+#endif
+			}
+		}
+	}
+}
